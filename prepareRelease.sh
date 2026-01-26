7aa9a5b4aa0bafb4697f810db7d23f4233534c9f
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2026-01-25 21:43:57 -0500

    test

--- prepareRelease.sh
+++ prepareRelease.sh
@@ -3,6 +3,6 @@ set -e
 
 # Delete current release tag
 if git show-ref --tags latest --quiet; then
-  gh release delete latest --cleanup-tag -y
+  gh release delete latest --cleanup-tag -y -R Dreeam-qwq/Velocity
 fi
 echo "🚀Ready for release"
